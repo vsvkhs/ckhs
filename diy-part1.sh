75369ba3c6a7af056fe6305ae5cef7db98ae6f69
vsvkhs <61653054+vsvkhs@users.noreply.github.com>
2022-04-05 03:26:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
+#git clone https://github.com/HoldOnBro/packages.git package/luci-app-adblock-plus.git
 git clone https://github.com/HoldOnBro/packages.git package/luci-app-adblock-plus
